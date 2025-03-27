f33be437db409a3558941233b661c62ae7dd0813
HW2 <24022473@vnu.edu.vn>
2025-03-27 13:25:09 +0700

    hello

--- Dictionary/src/TEST5.java
+++ Dictionary/src/TEST5.java
@@ -1,6 +1,6 @@
 public class TEST5 {
     public static void main(String[] args) {
         System.out.println("hhhhghghghssaa");
-        System.out.println("ccututut ");
+        System.out.println("Anh yeu em ");
     }
 }
