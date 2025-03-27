0841acd13df5d4abbd6293bba8a83321350fba56
HW2 <24022473@vnu.edu.vn>
2025-03-27 12:19:45 +0700

    ngngocanh

--- Dictionary/src/test.java
+++ Dictionary/src/test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Tét hhjnibhkhoiifoofofrofofffi");
+        System.out.println("Yeu Nguyen Ngoc Anh");
     }
 }
