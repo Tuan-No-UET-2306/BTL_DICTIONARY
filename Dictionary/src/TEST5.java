57d44cfdf773ae087e1973c060760c732f42cb97
HW2 <24022473@vnu.edu.vn>
2025-03-27 13:10:56 +0700

    anhtrung l

--- Dictionary/src/TEST5.java
+++ Dictionary/src/TEST5.java
@@ -1,6 +1,6 @@
 public class TEST5 {
     public static void main(String[] args) {
         System.out.println("hhhhghghghssaa");
-        System.out.println("cc ");
+        System.out.println("ccututut ");
     }
 }
