4cc4545fbb405cdcbfda3f92488d7b574832444a
toan3107 <24022467@vnu.edu.vn>
2025-03-27 12:23:02 +0700

    trung ngu

--- Dictionary/src/test.java
+++ Dictionary/src/test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Dit con me em trung" +
+        System.out.println("Dit con me em trunglon" +
                 "i");
     }
 }
