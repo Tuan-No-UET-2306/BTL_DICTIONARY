cc81c6e2443f5ee4c38dfade49b70f98654851bb
Tuan-No-UET-2306 <24022485@vnu.edu.vn>
2025-04-02 14:38:55 +0700

    new dictionary

---
+++ Dictionary/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.dictionary {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.dictionary to javafx.fxml;
+    exports com.example.dictionary;
+}
