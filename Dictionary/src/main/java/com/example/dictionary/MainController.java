f458d7e600cb09ecc55ac0fba5868ca236145dfa
Tuan-No-UET-2306 <24022485@vnu.edu.vn>
2025-04-04 21:32:11 +0700

    New Dictionary

---
+++ Dictionary/src/main/java/com/example/dictionary/MainController.java
@@ -0,0 +1,19 @@
+package com.example.dictionary;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import javax.swing.*;
+
+public class MainController {
+    @FXML
+    Button b1;
+    @FXML
+    Label l1;
+
+    public void setL1(ActionEvent event) {
+        l1.setText("abc");
+    }
+}
