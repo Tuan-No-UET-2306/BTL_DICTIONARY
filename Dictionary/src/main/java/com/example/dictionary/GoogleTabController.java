1b1ba64c5f4d06d793189f98849fc860e3b5f056
Tuan-No-UET-2306 <24022485@vnu.edu.vn>
2025-04-16 16:05:56 +0700

    done mainController and frontend of googletab

--- Dictionary/src/main/java/com/example/dictionary/GoogleTabController.java
+++ Dictionary/src/main/java/com/example/dictionary/GoogleTabController.java
@@ -1,6 +1,12 @@
 package com.example.dictionary;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 
 public class GoogleTabController {
+    @FXML
+    public Button exitTrans;
+    @FXML
+    public Button hoanDoiButton;
 }
--- Dictionary/src/main/java/com/example/dictionary/MainController.java
+++ Dictionary/src/main/java/com/example/dictionary/MainController.java
@@ -1,23 +1,109 @@
 package com.example.dictionary;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 import javax.swing.*;
+import java.io.IOException;
+import java.util.Objects;
 
-public class MainController {
+public class MainController implements EventHandler<ActionEvent> {
     @FXML
-    Button b1;
+    Button homeButton;
     @FXML
-    Label l1;
+    Button dicButton;
+    @FXML
+    Button transButton;
+    @FXML
+    Button gameButton;
 
-    public void setL1(ActionEvent event) {
-        l1.setText("abc");
 
-        System.out.println("abc");
+    @Override
+    public void handle(ActionEvent event) {
+            homeButton.setOnAction(e -> {
+                try {
+                    Parent nextStage = FXMLLoader.load(Objects.requireNonNull
+                            (getClass().getResource("home_tab.fxml")));
+                    Scene nextScene = new Scene(nextStage);
+
+                    // Tạo Stage mới
+                    Stage newStage = new Stage();
+                    newStage.setScene(nextScene);
+                    newStage.setTitle("HomeTab");
+                    newStage.show();
+
+                    // Đóng Stage cũ
+                    Stage currentStage = (Stage) homeButton.getScene().getWindow();
+                    currentStage.close();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+            });
+        dicButton.setOnAction(e -> {
+            try {
+                Parent nextStage = FXMLLoader.load(Objects.requireNonNull
+                        (getClass().getResource("main_dictionary.fxml")));
+                Scene nextScene = new Scene(nextStage);
+
+                // Tạo Stage mới
+                Stage newStage = new Stage();
+                newStage.setScene(nextScene);
+                newStage.setTitle("Dictionary");
+                newStage.show();
+
+                // Đóng Stage cũ
+                Stage currentStage = (Stage) dicButton.getScene().getWindow();
+                currentStage.close();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+
+        });
+        gameButton.setOnAction(e -> {
+            try {
+                Parent nextStage = FXMLLoader.load(Objects.requireNonNull
+                        (getClass().getResource("game_tab.fxml")));
+                Scene nextScene = new Scene(nextStage);
+
+                // Tạo Stage mới
+                Stage newStage = new Stage();
+                newStage.setScene(nextScene);
+                newStage.setTitle("Game");
+                newStage.show();
+
+                // Đóng Stage cũ
+                Stage currentStage = (Stage) gameButton.getScene().getWindow();
+                currentStage.close();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+
+        });
+            transButton.setOnAction(e -> {
+                try {
+                    Parent nextStage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("google_tab.fxml")));
+                    Scene nextScene = new Scene(nextStage);
+
+                    // Tạo Stage mới
+                    Stage newStage = new Stage();
+                    newStage.setScene(nextScene);
+                    newStage.setTitle("Google Tab");
+                    newStage.show();
+
+                    // Đóng Stage cũ
+                    Stage currentStage = (Stage) transButton.getScene().getWindow();
+                    currentStage.close();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            });
     }
-
-
 }
