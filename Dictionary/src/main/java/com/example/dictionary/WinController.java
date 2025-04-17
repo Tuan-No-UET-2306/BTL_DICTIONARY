6627d9ad58e85b64d756f8687cd5d6c2e1b5890e
Tuan-No-UET-2306 <24022485@vnu.edu.vn>
2025-04-17 17:29:40 +0700

    demo quizz game

---
+++ Dictionary/src/main/java/com/example/dictionary/WinController.java
@@ -0,0 +1,29 @@
+package com.example.dictionary;
+
+import Function.ChangeStage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class WinController implements EventHandler<ActionEvent>  {
+    @FXML
+    ImageView nextQues;
+
+
+    @Override
+    public void handle(ActionEvent event) {
+
+    }
+}
