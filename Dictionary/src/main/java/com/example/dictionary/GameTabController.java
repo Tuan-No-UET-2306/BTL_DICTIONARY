cf10af8fe8d13ee7c657a42da0bc15a1658d9ddc
Tuan-No-UET-2306 <24022485@vnu.edu.vn>
2025-04-17 14:09:04 +0700

    lam quizz_game

--- Dictionary/src/main/java/com/example/dictionary/GameTabController.java
+++ Dictionary/src/main/java/com/example/dictionary/GameTabController.java
@@ -1,11 +1,27 @@
 package com.example.dictionary;
 
+import Function.ChangeStage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
-public class GameTabController {
+public class GameTabController implements EventHandler<ActionEvent> {
     @FXML
     public Button quizzButton;
     @FXML
     public Button ctwButton;
+    @FXML
+    public Button exitGame;
+
+
+    @Override
+    public void handle(ActionEvent event) {
+        exitGame.setOnAction(e -> {
+            ChangeStage.changeStage(exitGame, "main.fxml", getClass());
+        });
+        quizzButton.setOnAction(e -> {
+            ChangeStage.changeStage(quizzButton,"quizz_game.fxml",getClass());
+        });
+    }
 }
