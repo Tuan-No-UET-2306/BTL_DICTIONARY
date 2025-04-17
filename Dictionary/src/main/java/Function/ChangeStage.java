962f1b482dba003ab638b77f6f63ccc2d93253f8
Tuan-No-UET-2306 <24022485@vnu.edu.vn>
2025-04-17 08:40:34 +0700

    Done changeStage toan cuc

---
+++ Dictionary/src/main/java/Function/ChangeStage.java
@@ -0,0 +1,32 @@
+package Function;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class ChangeStage {
+    public static void changeStage(Button a, String nameOfStage,Class<?> clazz) {
+        try {
+            Parent nextStage = FXMLLoader.load(Objects.requireNonNull
+                    (clazz.getResource(nameOfStage)));
+            Scene nextScene = new Scene(nextStage);
+
+            // Tạo Stage mới
+            Stage newStage = new Stage();
+            newStage.setScene(nextScene);
+            newStage.setTitle("BaChuTeEnglish");
+            newStage.show();
+
+            // Đóng Stage cũ
+            Stage currentStage = (Stage) a.getScene().getWindow();
+            currentStage.close();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
--- Dictionary/src/main/java/com/example/dictionary/MainController.java
+++ Dictionary/src/main/java/com/example/dictionary/MainController.java
@@ -1,5 +1,6 @@
 package com.example.dictionary;
 
+import Function.ChangeStage;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -22,86 +23,93 @@ public class MainController implements EventHandler<ActionEvent> {
     @FXML
     Button gameButton;
 
-
+//    public void initialize() {
+//        homeButton.setOnAction(e -> {
+//            ChangeStage.changeStage(homeButton, "home_tab.fxml", getClass());
+//        });
+//    }
     @Override
     public void handle(ActionEvent event) {
         homeButton.setOnAction(e -> {
-            try {
-                Parent nextStage = FXMLLoader.load(Objects.requireNonNull
-                        (getClass().getResource("home_tab.fxml")));
-                Scene nextScene = new Scene(nextStage);
-
-                // Tạo Stage mới
-                Stage newStage = new Stage();
-                newStage.setScene(nextScene);
-                newStage.setTitle("HomeTab");
-                newStage.show();
-
-                // Đóng Stage cũ
-                Stage currentStage = (Stage) homeButton.getScene().getWindow();
-                currentStage.close();
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
+//            try {
+//                Parent nextStage = FXMLLoader.load(Objects.requireNonNull
+//                        (getClass().getResource("home_tab.fxml")));
+//                Scene nextScene = new Scene(nextStage);
+//
+//                // Tạo Stage mới
+//                Stage newStage = new Stage();
+//                newStage.setScene(nextScene);
+//                newStage.setTitle("HomeTab");
+//                newStage.show();
+//
+//                // Đóng Stage cũ
+//                Stage currentStage = (Stage) homeButton.getScene().getWindow();
+//                currentStage.close();
+//            } catch (IOException ex) {
+//                throw new RuntimeException(ex);
+//            }
+            ChangeStage.changeStage(homeButton, "home_tab.fxml", getClass());
 
         });
         dicButton.setOnAction(e -> {
-            try {
-                Parent nextStage = FXMLLoader.load(Objects.requireNonNull
-                        (getClass().getResource("main_dictionary.fxml")));
-                Scene nextScene = new Scene(nextStage);
-
-                // Tạo Stage mới
-                Stage newStage = new Stage();
-                newStage.setScene(nextScene);
-                newStage.setTitle("Dictionary");
-                newStage.show();
-
-                // Đóng Stage cũ
-                Stage currentStage = (Stage) dicButton.getScene().getWindow();
-                currentStage.close();
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-
+//            try {
+//                Parent nextStage = FXMLLoader.load(Objects.requireNonNull
+//                        (getClass().getResource("main_dictionary.fxml")));
+//                Scene nextScene = new Scene(nextStage);
+//
+//                // Tạo Stage mới
+//                Stage newStage = new Stage();
+//                newStage.setScene(nextScene);
+//                newStage.setTitle("Dictionary");
+//                newStage.show();
+//
+//                // Đóng Stage cũ
+//                Stage currentStage = (Stage) dicButton.getScene().getWindow();
+//                currentStage.close();
+//            } catch (IOException ex) {
+//                throw new RuntimeException(ex);
+//            }
+            ChangeStage.changeStage(dicButton,"main_dictionary.fxml", getClass());
         });
         gameButton.setOnAction(e -> {
-            try {
-                Parent nextStage = FXMLLoader.load(Objects.requireNonNull
-                        (getClass().getResource("game_tab.fxml")));
-                Scene nextScene = new Scene(nextStage);
-
-                // Tạo Stage mới
-                Stage newStage = new Stage();
-                newStage.setScene(nextScene);
-                newStage.setTitle("Game");
-                newStage.show();
-
-                // Đóng Stage cũ
-                Stage currentStage = (Stage) gameButton.getScene().getWindow();
-                currentStage.close();
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-
+//            try {
+//                Parent nextStage = FXMLLoader.load(Objects.requireNonNull
+//                        (getClass().getResource("game_tab.fxml")));
+//                Scene nextScene = new Scene(nextStage);
+//
+//                // Tạo Stage mới
+//                Stage newStage = new Stage();
+//                newStage.setScene(nextScene);
+//                newStage.setTitle("Game");
+//                newStage.show();
+//
+//                // Đóng Stage cũ
+//                Stage currentStage = (Stage) gameButton.getScene().getWindow();
+//                currentStage.close();
+//            } catch (IOException ex) {
+//                throw new RuntimeException(ex);
+//            }
+            ChangeStage.changeStage(gameButton,"game_tab.fxml", getClass());
         });
         transButton.setOnAction(e -> {
-            try {
-                Parent nextStage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("google_tab.fxml")));
-                Scene nextScene = new Scene(nextStage);
-
-                // Tạo Stage mới
-                Stage newStage = new Stage();
-                newStage.setScene(nextScene);
-                newStage.setTitle("Google Tab");
-                newStage.show();
-
-                // Đóng Stage cũ
-                Stage currentStage = (Stage) transButton.getScene().getWindow();
-                currentStage.close();
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
+//            try {
+//                Parent nextStage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("google_tab.fxml")));
+//                Scene nextScene = new Scene(nextStage);
+//
+//                // Tạo Stage mới
+//                Stage newStage = new Stage();
+//                newStage.setScene(nextScene);
+//                newStage.setTitle("Google Tab");
+//                newStage.show();
+//
+//                // Đóng Stage cũ
+//                Stage currentStage = (Stage) transButton.getScene().getWindow();
+//                currentStage.close();
+//            } catch (IOException ex) {
+//                throw new RuntimeException(ex);
+//            }
+//        });
+            ChangeStage.changeStage(transButton,"google_tab.fxml", getClass());
         });
-    }
+}
 }
