d03fb620e82ba5bb341ef45409a7e1e1e83c8c8a
Tuan-No-UET-2306 <24022485@vnu.edu.vn>
2025-05-06 20:01:44 +0700

    Done homeTab

--- Dictionary/src/main/java/com/example/dictionary/HomeTabController.java
+++ Dictionary/src/main/java/com/example/dictionary/HomeTabController.java
@@ -1,4 +1,16 @@
 package com.example.dictionary;
 
+import Function.ChangeStage;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.awt.event.ActionEvent;
+
 public class HomeTabController {
+    @FXML
+    Button backButton;
+
+    public void handleBack(javafx.event.ActionEvent event) {
+        ChangeStage.changeStage(backButton, "main.fxml", getClass());
+    }
 }
